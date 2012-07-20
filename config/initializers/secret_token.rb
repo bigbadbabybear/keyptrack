e8ad23fa8ea25f37af0b1b657d540c7f0b7e8963
ErwinBatucan <erwinbatucan@gmail.com>
2012-07-20 14:00:37 +0800

    clean rails

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Keyptrack::Application.config.secret_token = 'e6e4e116b844066d95ed78d60565c1af660e84dcf2384a279352a541bc5bc5b3eb5d2762850511fe35d8b26dcb2cf37f51b95f9298e29f4f1266c99487f8f789'
+Keyptrack::Application.config.secret_token = 'e60a5c3427b23b5c1a2f7dcbe936d83f041d524da5be8c1b836f28d16ff932d2638dc0049730ad97412dbdc3e2e9c583152e061e4258b41315579f64ff665da3'
