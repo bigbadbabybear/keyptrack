32f3331c6ac67a0db8a8e7999b4525c0bc4a473a
ErwinBatucan <erwinbatucan@gmail.com>
2012-07-20 13:55:35 +0800

    initial commit - clean rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keyptrack::Application.config.secret_token = 'e6e4e116b844066d95ed78d60565c1af660e84dcf2384a279352a541bc5bc5b3eb5d2762850511fe35d8b26dcb2cf37f51b95f9298e29f4f1266c99487f8f789'
