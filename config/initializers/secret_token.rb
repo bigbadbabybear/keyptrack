8fe520ebf5c6f894765ab28b30da61e6b0e40f3d
ErwinBatucan <erwinbatucan@gmail.com>
2012-07-25 11:30:11 +0800

    now using postgreSQL for heroku

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Keyptrack::Application.config.secret_token = 'e60a5c3427b23b5c1a2f7dcbe936d83f041d524da5be8c1b836f28d16ff932d2638dc0049730ad97412dbdc3e2e9c583152e061e4258b41315579f64ff665da3'
+Keyptrack::Application.config.secret_token = '1785ed86c3173d6f60cf476de4171e81906c3b6fab6eea4b8c03dba99065e44a82acc339701c3c2574302cf1d41316712799d8df1b02da376a9aac880def6373'
